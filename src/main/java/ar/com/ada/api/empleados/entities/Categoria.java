8c363cea39a2a399235fddbc08ea7259ce2e0129
iararoldan99 <iarah_debera@hotmail.com>
2020-06-20 22:10:18 -0300

    Comenzamos a construir el esqueleto de la API: Se crearon las entidades Empleado y Categoria, junto a sus respectivos services, repos y controllers

---
+++ src/main/java/ar/com/ada/api/empleados/entities/Categoria.java
@@ -0,0 +1,56 @@
+package ar.com.ada.api.empleados.entities;
+
+import java.math.BigDecimal;
+import java.util.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "categoria")
+public class Categoria {
+    @Column(name = "categoria_id")
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int categoriaId;
+    private String nombre;
+    @Column(name = "sueldo_base")
+    private BigDecimal sueldoBase;
+    // mappedBy busca el atributo 'categoria' en la clase empleado
+    // cascadetypeALL trae todos los empleados de esa categoria 
+    // fetch obliga a que traiga todos de una. el lazy solo los trae cuando se llama, no se cargan de una
+    @OneToMany(mappedBy = "categoria", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<Empleado> empleados = new ArrayList<>();
+
+    public int getCategoriaId() {
+        return categoriaId;
+    }
+
+    public void setCategoriaId(int categoriaId) {
+        this.categoriaId = categoriaId;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public BigDecimal getSueldoBase() {
+        return sueldoBase;
+    }
+
+    public void setSueldoBase(BigDecimal sueldoBase) {
+        this.sueldoBase = sueldoBase;
+    }
+
+    public List<Empleado> getEmpleados() {
+        return empleados;
+    }
+
+    public void setEmpleados(List<Empleado> empleados) {
+        this.empleados = empleados;
+    }
+
+}
